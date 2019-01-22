a38dcc9bbd4c455a322c45a26bd5950dd2c4d57b
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-01-22 23:58:39 +0100

    update audio function

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 window.addEventListener('keydown', function(e) {
   console.log(e);
+  const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
+  if (!audio) return; // stop the function to running all together
+  console.log(audio);
 });
